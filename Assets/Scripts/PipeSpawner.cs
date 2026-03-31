1247adce23b697e5f459a059731b22d0e98495db
Plokhoy <fak-mama@mail.ru>
2026-04-01 00:30:54 +0300

    spawn

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject Pipe;
+    [SerializeField] private float _timer = 0f;
+    private float _minY = -2f;
+    private float _maxY = 2f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        _timer += Time.deltaTime;
+        if (_timer >= 4f)
+        {
+            new Vector3 (transform.position.x, Random.Range(_minY, _maxY), transform.position.z);
+            Instantiate(Pipe, transform.position, Quaternion.identity);
+             
+            _timer = 0;
+        }
+
         
     }
 }
