8dd8b92f50c55b72cc4af3e3c23ce0ffc8124797
Plokhoy <fak-mama@mail.ru>
2026-06-24 00:15:48 +0300

    need to fix this

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -4,10 +4,28 @@ public class Pipe : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
 
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     public void Update()
     {
+<<<<<<< Updated upstream
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
         if (transform.position.x < -10)
+=======
+        if (GameManager.Instance._gameState == GameManager.GameState.Dead)
+        {
+            return;
+        }
+        if (GameManager.Instance._gameState == GameManager.GameState.Playing)
+        {
+            rb.MovePosition(rb.position + Vector2.left * moveSpeed * Time.deltaTime);
+        }
+        if (rb.position.x < -10)
+>>>>>>> Stashed changes
         {
             Destroy (gameObject);
         }
--- Assets/Scripts/ScoreZone.cs
+++ Assets/Scripts/ScoreZone.cs
@@ -1,9 +1,21 @@
+
 using UnityEngine;
 
 public class ScoreZone : MonoBehaviour
 {
+    bool _point = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+<<<<<<< Updated upstream
         GameManager.Instance.AddScore();
+=======
+        
+        Debug.Log("Воёл в оюъект" + collision.gameObject.name); 
+        if (collision.CompareTag("Player") && (_point == false))
+        {
+            _point = true;
+            GameManager.Instance.AddScore();
+        }
+>>>>>>> Stashed changes
     }
 }
